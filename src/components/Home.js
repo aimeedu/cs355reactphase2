9d7531b2f9fadc38ff1c53ea15f2e303c13057b3
aimeedu <aimeedu11@gmail.com>
2019-11-11 23:28:34 -0500

    phase2 v3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
+const style = {
+    marginTop: '200px',
+}
 
 const Home = () => {
     return (
-        <div><h2>This is my home page for CS 355 !</h2></div>
+        <div style={style}><h2>This is my home page for CS 355 !</h2></div>
     )
 }
 export default Home;
