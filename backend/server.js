09ddaee978a135c49ea5c2650c29a0c7da2667ff
aimeedu <aimeedu11@gmail.com>
2019-12-08 09:04:45 -0500

    insert into word table

--- backend/server.js
+++ backend/server.js
@@ -54,25 +54,46 @@ app.get('/admin', db.getSearchTable);
 
 app.post('/admin', (req,res)=>{
     const getURL = req.body.inputURL;
-    //print the url in the terminal
     let title, description, lastModified;
     // need to fix the web crawler. for some website, it can't extracting data from certain field. try apple.com
     axios.get(getURL).then((res) => {
+
         if(res.status === 200) {
             const html = res.data;
             const $ = cheerio.load(html);
             title = $('title').text();
-            console.log(title);
-            description = $('meta').filter((i, elem) => elem.attribs.name === 'description')[0].attribs.content;
-            console.log(description);
+            if (title != null) title.trim();
+
+            description = $('meta[name="description"]').attr('content');
+            if (description != null) {
+                description.trim();
+            } else{
+                /* If the description is unavailable set the description to the title */
+                description = title;
+            }
             lastModified = res.headers['last-modified'];
-            console.log(lastModified);
         }
-        pool.query('INSERT INTO page (url, title, description, lastModified) VALUES ($1, $2, $3, $4)', [getURL, title, description, lastModified], (error) => {
-        if (error) {
-            throw error
+
+        // /*If the lastModified is unavailable set the lastModified to the current time stamp */
+
+        /* We retrieve the current time from the webpage when it was last modified, if the page
+        * does not have a lastModified value hence it is null we have two cases
+        * where if it is null we allow it to be a default value to be Current_Timestamp
+        * else it is the default time-stamp of the webpage's host time*/
+        if (lastModified != null) {
+            pool.query('INSERT INTO page (url, title, description, lastModified) VALUES ($1, $2, $3, $4)', [getURL, title, description, lastModified], (error) => {
+                if (error) {
+                    throw error
+                }
+            });
+        } else {
+            pool.query('INSERT INTO page (url, title, description, lastModified) VALUES ($1, $2, $3, CURRENT_TIMESTAMP)', [getURL, title, description], (error) => {
+                if (error) {
+                    throw error
+                }
+            });
         }
-    })
+
     })
 });
 
@@ -83,55 +104,67 @@ app.get('/custom', (req, res) => {
 
 // the crawler gets all the plain text from the body of a web page --------------------------------------------------------
 const URL = 'https://en.wikipedia.org/wiki/London';
-// write to a txt file. Pizon's code can parse text file, but need to clean up the file first.
-// the txt file is going to output under the backend folder.
+let counts = {};
+let keys = [];
+/** We create an array parsedWords which stores the words into an array
+ * which will be used later to insert into the database based on their indices */
+let parsedWords = [];
 
 const countWords = request(URL, function (err, res, body) {
     if(err) {
         console.log(err, "error occured while hitting URL");
     }
     else {
-        // let arr =[];
         let $ = cheerio.load(body);
         let txt = $('body').text();
-        // arr.push(txt);
         setup(txt);
-        console.log(counts);
-
-        // fs.writeFile('data.txt', txt, function (err) {
-        //     if(err) {
-        //         console.log(err);
-        //     }
-        //     else{
-        //         console.log("success");
-        //     }
-        // });
+        // Loops through all the words in the array of parsedWords for each word in their indices
+        // It inserts into the database
+        for (let i = 0; i < parsedWords.length; i++) {
+            pool.query('INSERT INTO word (wordName) VALUES ($1)', [parsedWords[i]], (error) => {
+                if (error) {
+                    throw error
+                }
+            });
+            pool.query('INSERT INTO page_word (wordname) VALUES ($1)', [parsedWords[i]], (error) => {
+                if (error) {
+                    throw error
+                }
+            });
+        }
+        for(let i = 0; i<counts.length; i++) {
+            pool.query('INSERT INTO page_word (freq) VALUES ($1)', [counts[i]], (error) => {
+                if (error) {
+                    throw error
+                }
+            });
+        }
     }
 });
 
-let counts = {};
-let keys = [];
+
 function setup (txt){
-    // let allwords = txt.join("\n");
+    //todo: Take care of words like "can't" and "don't"
+    //* It takes all the words and splits by anything that are not letters  */
     let tokens  = txt.split(/\W+/);
     for (let i = 0; i< tokens.length; i++){
-        let word = tokens[i]
-        if (!/\d+/.test(word)) {
+        let word = tokens[i].trim();//.toLowerCase();
+        // This is ignoring all digits in the text, so if it is not a digit then we continue
+        const pattern = new RegExp(/\d+/);
+        if (!pattern.test(word)) {
+            // If a word is undefined then we add that means it hasn't appeared yet
             if (counts[word] === undefined) {
+                // Since the word has appeared for the first time we make count = 1.
                 counts[word] = 1;
-                keys.push(word);
+                keys.push(word.trim());
+                parsedWords.push(word);
             } else {
                 counts[word] = counts[word] + 1;
             }
         }
     }
-    // function compare(a,b){
-    //     var countA = counts[a];
-    //     var countB = counts[b];
-    //     return countB - countA;
-    // }
-    // keys.sort(compare);
 }
+
 
 
 
